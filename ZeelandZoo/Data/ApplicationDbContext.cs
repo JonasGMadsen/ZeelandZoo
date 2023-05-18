25b0f167e0d05f33eb80d2979cc39dba90043513
JonasGMadsen <Jonas6070@hotmail.com>
2023-05-18 22:50:06 +0200

    Basic User fixed. Updated user table with name. Now works

--- ZeelandZoo/Data/ApplicationDbContext.cs
+++ ZeelandZoo/Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ZeelandZoo.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
     }
+
+
 }
---
+++ ZeelandZoo/Data/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ZeelandZoo.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+
+
+}
