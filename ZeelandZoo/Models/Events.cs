acba687ee74a8bc1f8bd8665e284e465a46db95e
JonasGMadsen <Jonas6070@hotmail.com>
2023-05-25 18:17:12 +0200

    Small html changes

--- ZeelandZoo/Models/Events.cs
+++ ZeelandZoo/Models/Events.cs
@@ -5,11 +5,16 @@ namespace ZeelandZoo.Models
     public class Events
     {
         public int Id { get; set; }
+
+        [Required]
        public string? Name { get; set; }
-        public string? Description { get; set; }
-        public DateTime? EventDate { get; set;}
-        public decimal? EventPrice { get; set; }
-        public int? EventAttendanceLimit { get; set; }
+       
+       public string? Description { get; set; }
+        
+        [Required]
+       public DateTime? EventDate { get; set;}
+       public decimal? EventPrice { get; set; }
+       public int? EventAttendanceLimit { get; set; }
 
     }
 }
